62b51a18872cf6942ad3757ce488bdbb834f0a44
alexcarhen <alejandrocardonahe@gmail.com>
2021-10-25 21:27:41 -0500

    primer commit de pruebas
    se realiza cambio en mensaje saludo2 "hola primer commit"

--- src/main/java/saludo2.java
+++ src/main/java/saludo2.java
@@ -29,7 +29,7 @@ public class saludo2 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Hola");
+        jLabel1.setText("Hola primer commit");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
